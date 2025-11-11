f209248d4fc81328bf57fe009d1c9638f48c735e
Anniballi-Andrea <andreanniballi@gmail.com>
2025-11-11 11:37:25 +0100

    start work

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+
+  const film = [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+  ]
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
