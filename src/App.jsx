03a7104890edbbc51afbacce3e54c8a474d6b847
Anniballi-Andrea <andreanniballi@gmail.com>
2025-11-11 16:33:21 +0100

    add the search by title section

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,13 @@ function App() {
     { title: 'Interstellar', genre: 'Fantascienza' },
     { title: 'Pulp Fiction', genre: 'Thriller' },
   ]
-  const [category, setCategory] = useState('start')
+  const [category, setCategory] = useState('All')
+  const [title, setTitle] = useState('')
   const [filtred, setFiltred] = useState(film)
 
   useEffect(() => {
 
-    if (category === 'start') {
+    if (category === 'All') {
       setFiltred(film)
 
       console.log('start')
@@ -30,6 +31,19 @@ function App() {
     }
   }, [category])
 
+  useEffect(() => {
+    if (title === '') {
+      setFiltred(film)
+
+    } else {
+      const filtredFilm = film.filter((el) => el.title.toLowerCase() === title.toLowerCase())
+      setFiltred(filtredFilm)
+
+    }
+
+
+  }, [title])
+
 
 
 
@@ -38,22 +52,29 @@ function App() {
   return (
     <>
 
-      <div className="container mt-5">
-        <select
-          className="form-select"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}>
-          <option value="start"
-            onChange={(e) => setCategory(e.target.value)}>-</option>
-          <option value="Fantascienza"
-            onChange={(e) => setCategory(e.target.value)}>Fantascienza</option>
-          <option value="Thriller"
-            onChange={(e) => setCategory(e.target.value)}>Thriller</option>
-          <option value="Romantico"
-            onChange={(e) => setCategory(e.target.value)}>Romantico</option>
-          <option value="Azione"
-            onChange={(e) => setCategory(e.target.value)}>Azione</option>
-        </select>
+      <div className="container mt-5 ">
+        <div className="d-flex">
+          <select
+            className="form-select"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}>
+            <option value="All"
+              onChange={(e) => setCategory(e.target.value)}>All</option>
+            <option value="Fantascienza"
+              onChange={(e) => setCategory(e.target.value)}>Fantascienza</option>
+            <option value="Thriller"
+              onChange={(e) => setCategory(e.target.value)}>Thriller</option>
+            <option value="Romantico"
+              onChange={(e) => setCategory(e.target.value)}>Romantico</option>
+            <option value="Azione"
+              onChange={(e) => setCategory(e.target.value)}>Azione</option>
+          </select>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Cerca in base al titolo" />
+        </div>
 
         <div className="card">
           <MovieList array={filtred} />
